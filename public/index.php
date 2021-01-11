a448c13fff1548d8eae4bb94d49ced615f4f46d6
João Paulo <joaopaulozangrande@gmail.com>
2021-01-11 19:16:02 -0300

    i create two controllers

--- public/index.php
+++ public/index.php
@@ -1,11 +1,17 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use Alura\Cursos\Controller\{ListarCursos,FormularioInsercao};
+
 switch ($_SERVER['PATH_INFO']){
     case '/listar-cursos':
-        require 'listar-cursos.php';
+        $controlador = new ListarCursos();
+        $controlador->processaRequisicao();
         break;
     case '/novo-curso':
-        require "formulario-novo-curso.php";
+        $controlador = new FormularioInsercao();
+        $controlador->processaRequisicao();
         break;
     default:
         echo "Erro 404";
