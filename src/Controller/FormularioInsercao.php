5b95cda8d5a98bbeddb76b9c74a37f6744cf1d71
João Paulo <joaopaulozangrande@gmail.com>
2021-01-11 20:44:26 -0300

    i partition th HTML

--- src/Controller/FormularioInsercao.php
+++ src/Controller/FormularioInsercao.php
@@ -6,6 +6,7 @@ class FormularioInsercao implements InterfaceControladorRequisicao
 {
     public function processaRequisicao(): void
     {
+        $titulo = "Novo curso";
         require __DIR__ . '/../../view/cursos/formulario.php';
     }
 }
