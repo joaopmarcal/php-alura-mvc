3f16310e5307b8a0ee784ae2f6fd8e40a12603b3
João Paulo <joaopaulozangrande@gmail.com>
2021-01-20 19:09:00 -0300

    i add session in the code

--- src/Controller/RealizarLogin.php
+++ src/Controller/RealizarLogin.php
@@ -42,6 +42,7 @@ class RealizarLogin implements InterfaceControladorRequisicao
             echo "E-mail ou senha inválidos";
             return;
         }
+        $_SESSION['logado'] = true;
 
         header('Location: /listar-cursos');
     }
